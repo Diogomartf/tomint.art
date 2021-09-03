e4880e28dddeb3fc24892772453790a88ba4a221
Diogo <diogo.mart.ferreira@gmail.com>
2021-09-03 17:25:53 +0100

    Add favicon

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,32 @@ export default function Home({
           name="description"
           content="Find active and upcoming NTFs to mint."
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <img
         className="absolute top-0 right-0 -z-10"
